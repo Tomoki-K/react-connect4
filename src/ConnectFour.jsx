00e3d31caf3b423f9065b8043a8d1259dfb7a279
Tomoki-K <tomofuma@gmail.com>
2018-07-08 21:19:16 +0900

    reset after gameset

--- src/ConnectFour.jsx
+++ src/ConnectFour.jsx
@@ -12,12 +12,31 @@ export default class ConnectFour extends React.Component {
 		this.state = {
 			board: [...Array(X_LEN)].map(e => Array(Y_LEN).fill(0)),
 			player: 1,
-			message: "player1's turn"
+			message: "player1's turn",
+			gameset: false
+		}
+	}
+
+	initialize() {
+		this.setState({
+			board: [...Array(X_LEN)].map(e => Array(Y_LEN).fill(0)),
+			player: 1,
+			message: "player1's turn",
+			gameset: false
+		});
+	}
+
+	handleClick(x) {
+		if (this.state.gameset) {
+			this.initialize();
+		} else {
+			this.setPiece(x);
 		}
 	}
 
 	setPiece(x) {
 		let settable = true;
+		let last_x, last_y;
 		let newboard = this.state.board
 
 		// validate choice
@@ -27,34 +46,46 @@ export default class ConnectFour extends React.Component {
 		let newRow = this.state.board[x].reverse().map((e, y) => {
 			if (e == 0 && settable) {
 				settable = false;
-				this.scanForWin(x, Y_LEN - y - 1);
+				last_x = x;
+				last_y = Y_LEN - y - 1;
 				return this.state.player;
 			} else {
 				return e;
 			}
 		});
 		newboard[x] = newRow.reverse();
+
 		let nextPlayer = this.state.player == 1 ? 2 : 1;
-		this.setState({
-			board: newboard,
-			player: nextPlayer,
-			message: `player${nextPlyer}'s turn`
-		});
+		if (this.isGameSet(last_x, last_y)) {
+			this.setState({
+				board: newboard,
+				message: `player${this.state.player} wins!`,
+				gameset: true
+			});
+		} else {
+			this.setState({
+				board: newboard,
+				player: nextPlayer,
+				message: `player${nextPlayer}'s turn`,
+				gameset: false
+			});
+		}
 	}
 
-	scanForWin(x, y) {
+	isGameSet(x, y) {
 		const directions = [
 			[1, 1], //[＼]方向
 			[0, 1], //[│]方向
 			[1, 0], //[─]方向
 			[-1, 1] //[／]方向
 		]
+		let flg = false;
 		directions.forEach((d) => {
 			if (this.scanDirection(x, y, d) == WINCNT) {
-				alert(`player${this.state.player} wins!`)
-				location.reload();
+				flg = true;
 			}
 		});
+		return flg;
 	}
 
 	scanDirection(x_id, y_id, dir) {
@@ -108,7 +139,7 @@ export default class ConnectFour extends React.Component {
                       <td key={`${y}-${x}`}
                         id={`${y}-${x}`}
                         className={`player${board[y][x]}`}
-                        onClick={(e) => this.setPiece(x)}>
+                        onClick={(e) => this.handleClick(x)}>
                         {x + "," + y}
                       </td>
                     );
