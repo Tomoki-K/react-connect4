ca33510bb9a0e8cb86a67a001e5ca5d34299cd3c
Tomoki-K <tomofuma@gmail.com>
2018-07-08 14:49:34 +0900

    init

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+class App extends React.Component {
+  render(){
+    return(
+      <div>
+        <h1>App</h1>
+      </div>
+    );
+  }
+}
+
+ReactDom.render(<App/>, document.getElementById("app"));
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    'app': './src/app.jsx',
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].bundle.js'
+  },
+
+  module: {
+    rules: [{
+      test: /\.jsx$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+
+  plugins: []
+}
